92e8dd54ac197e15d9c7a8e20f2489e2444b9880
iwint <iwint.cseasnsct@gmail.com>
2022-04-23 15:30:51 +0530

    Corrections

--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -1,19 +1,92 @@
-import React from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import { React, useState } from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import SubMenu from "./SubMenu";
+import { HiChevronDown, HiChevronUp } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import Data from "./SidebarData";
 
-function Sidebar() {
+function Sidebar({ sideBar }) {
+  const [collapse, setCollapse] = useState(false);
+
+  const toggleDropdown = () => setCollapse(!collapse);
+
   return (
-    <Navbar
-      color="#ffffff"
-      style={{
-        backgroundColor: "#ffffff",
-        width: "250px",
-        height: "100vh",
-      }}
-    >
-      Hello
-    </Navbar>
+    <>
+      {sideBar && (
+        <Navbar
+          style={{
+            backgroundColor: "#ffffff",
+            width: "250px",
+            height: "100vh",
+          }}
+        >
+          <NavbarBrand
+            className="px-3 my-2"
+            style={{ color: "#5735d2", fontWeight: "bold", fontSize: "20px" }}
+          >
+            <img
+              src="/logo.png"
+              alt=""
+              style={{ width: "150px", height: "50px" }}
+            />
+            <hr style={{ width: "200px" }} />
+          </NavbarBrand>
+        </Navbar>
+      )}
+    </>
   );
 }
 
 export default Sidebar;
+
+// const SideBar = ({ isOpen, toggle }) => (
+//   <div className={classNames("sidebar", { "is-open": isOpen })}>
+//     <div className="sidebar-header">
+//       <span color="info" onClick={toggle} style={{ color: "#fff" }}>
+//         &times;
+//       </span>
+//       <h3>Bootstrap Sidebar</h3>
+//     </div>
+//     <div className="side-menu">
+//       <Nav vertical className="list-unstyled pb-3">
+//         <p>Dummy Heading</p>
+//         <SubMenu title="Home" icon={faHome} items={submenus[0]} />
+//         <NavItem>
+//           <NavLink tag={Link} to={"/about"}>
+//             <FontAwesomeIcon icon={faBriefcase} className="mr-2" />
+//             About
+//           </NavLink>
+//         </NavItem>
+//         <SubMenu title="Pages" icon={faCopy} items={submenus[1]} />
+//         <NavItem>
+//           <NavLink tag={Link} to={"/pages"}>
+//             <FontAwesomeIcon icon={faImage} className="mr-2" />
+//             Portfolio
+//           </NavLink>
+//         </NavItem>
+//         <NavItem>
+//           <NavLink tag={Link} to={"/faq"}>
+//             <FontAwesomeIcon icon={faQuestion} className="mr-2" />
+//             FAQ
+//           </NavLink>
+//         </NavItem>
+//         <NavItem>
+//           <NavLink tag={Link} to={"/contact"}>
+//             <FontAwesomeIcon icon={faPaperPlane} className="mr-2" />
+//             Contact
+//           </NavLink>
+//         </NavItem>
+//       </Nav>
+//     </div>
+//   </div>
+// );
+
+// export default SideBar;
--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -6,74 +6,38 @@ import {
   Input,
   InputGroup,
   InputGroupText,
+  Nav,
+  Navbar,
+  NavItem,
   Row,
 } from "reactstrap";
 import { BiBell } from "react-icons/bi";
 import { RiSearch2Line } from "react-icons/ri";
 import { FaAlignLeft } from "react-icons/fa";
 import { RiLightbulbFlashLine } from "react-icons/ri";
-import User from "./User";
 import { Link } from "react-router-dom";
+import Sidebar from "../SideBar/Sidebar";
 
-function Topbar(toggle) {
+function Topbar({ toggle, sideBar }) {
   return (
     <>
-      <Container fluid className="p-3 px-0 mx-3 ">
-        <Row>
-          <Col lg={1} xs={2}>
-            <FaAlignLeft
-              color="#5735d2"
-              size="40px"
-              style={{
-                backgroundColor: "#ffffff",
-                padding: "8px",
-                borderRadius: "5px",
-              }}
-              onClick={toggle}
-            />
-          </Col>
-          <Col md={5} xs={7}>
-            <InputGroup>
-              <InputGroupText
-                style={{
-                  backgroundColor: "#ffffff",
-                  border: "none",
-                  borderColor: "#ffffff",
-                }}
-              >
-                <RiSearch2Line color="#8D8D8E" style={{ fontWeight: "bold" }} />
-              </InputGroupText>
-              <Input
-                style={{
-                  backgroundColor: "#ffffff",
-                  color: "#8D8D8E",
-                  border: "none",
-                  fontSize: "14px",
-                }}
-                className="p-2 input"
-                placeholder="Search anything"
-              />
-            </InputGroup>
-          </Col>
-          <Col md={2} lg={1} className="ms-lg-auto mx-2 d-none d-md-block">
-            <RiLightbulbFlashLine
-              color="#09090a"
-              className="p-1"
-              size="32px"
-              style={{ backgroundColor: "#eceef5", borderRadius: "5px" }}
-            />
-            <BiBell
-              color="#09090a"
-              className="p-1 ms-3"
-              size="32px"
-              style={{ backgroundColor: "#eceef5", borderRadius: "5px" }}
-            />
-          </Col>
-          <Col md={2} xs={3} sm={2}>
-            <User />
-          </Col>
-        </Row>
-      </Container>
+      <Navbar
+        expand="md"
+        className="topbar p-3 px-0 mx-3 col-12"
+        style={{
+          height: "100px",
+          width: "100%",
+          position: "relative",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <Nav>
+          <NavItem className="navItem">
+            <FaAlignLeft onClick={toggle} />
+          </NavItem>
+        </Nav>
+      </Navbar>
     </>
   );
 }
