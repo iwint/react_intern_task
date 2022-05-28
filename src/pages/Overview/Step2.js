07136547924115bdf212daff16e66301d9329167
iwint <iwint.cseasnsct@gmail.com>
2022-05-28 22:55:27 +0530

    CarData

--- src/pages/Overview/Step2.js
+++ src/pages/Overview/Step2.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Col, Container, Row } from "reactstrap";
 import Popup from "../Form/Popup";
 import Driverdetails from "./Driverdetails";
+import { CarData } from "./Step2Data";
 
 function Step2() {
   const dataStore = JSON.parse(localStorage.getItem("Data"));
@@ -105,6 +106,82 @@ function Step2() {
         </Button>
         <Driverdetails isOpen={isOpen} setisOpen={setisOpen} />
       </Col>
+      <Col className="Header mt-4">
+        <h1 className="header-title">Price Estimation</h1>
+      </Col>
+      <Row>
+        {" "}
+        {CarData.map((item, index) => {
+          return (
+            <Col lg={6}>
+              <Col className="d-flex flex-column justify-content-center align-items-center">
+                <img
+                  src={item.image}
+                  alt=""
+                  style={{ width: "120px", height: "100px" }}
+                />
+                <h6>{item.name}</h6>
+                <p className="text-muted">{item.seater}</p>
+              </Col>
+              <div></div>
+              <Col>
+                <Row className="trip-details ">
+                  <Col className="detail ">Estimate Distance</Col>
+                  <Col className="detail ">{item.estimate}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Base Fare (3 km)</Col>
+                  <Col className="detail ">{item.fare}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Extra km Price</Col>
+                  <Col className="detail ">{item.extra}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail "> Price AC / Km</Col>
+                  <Col className="detail ">{item.price}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">
+                    Remaining Fare (3.10 km * 19.05)
+                  </Col>
+                  <Col className="detail ">{item.rfare}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Convenience Fee</Col>
+                  <Col className="detail ">{item.fee}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">GST (CGST 2.5% + SGST 2.5%)</Col>
+                  <Col className="detail ">{item.gst}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Promo Code</Col>
+                  <Col className="detail " style={{ color: "#3FCA7E" }}>
+                    {item.promo}
+                  </Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Discount</Col>
+                  <Col className="detail ">{item.discount}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Total w/ discount</Col>
+                  <Col className="detail ">{item.total}</Col>
+                </Row>
+                <Row className="trip-details">
+                  <Col className="detail ">Driver Count</Col>
+                  <Col className="detail ">{item.count}</Col>
+                </Row>
+              </Col>
+
+              <Col>
+                <Button className="submit">Book Now</Button>
+              </Col>
+            </Col>
+          );
+        })}
+      </Row>
     </Container>
   );
 }
